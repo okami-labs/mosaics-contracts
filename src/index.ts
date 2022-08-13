e20e720ef71c411deade4b7376bf6aa90b143397
Adam Wolf <wolfadam14@gmail.com>
2022-08-12 18:20:49 -0700

    fix index exports for contract abis

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-export { default as MosaicsAuctionHouseABI } from 'abi/contracts/MosaicsAuctionHouse.sol/MosaicsAuctionHouse.json'
-export { default as MosaicsPassTokenABI } from 'abi/contracts/MosaicsPassToken.sol/MosaicsPassToken.json'
-export { default as MosaicsTokenABI } from 'abi/contracts/MosaicsToken.sol/MosaicsToken.json'
+export { default as MosaicsAuctionHouseABI } from '../abi/contracts/MosaicsAuctionHouse.sol/MosaicsAuctionHouse.json'
+export { default as MosaicsPassTokenABI } from '../abi/contracts/MosaicsPassToken.sol/MosaicsPassToken.json'
+export { default as MosaicsTokenABI } from '../abi/contracts/MosaicsToken.sol/MosaicsToken.json'
 export { MosaicsAuctionHouse__factory as MosaicsAuctionHouseFactory } from '../typechain/factories/contracts/MosaicsAuctionHouse__factory'
 export { MosaicsPassToken__factory as MosaicsPassTokenFactory } from '../typechain/factories/contracts/MosaicsPassToken__factory'
 export { MosaicsToken__factory as MosaicsTokenFactory } from '../typechain/factories/contracts/MosaicsToken__factory'
+
